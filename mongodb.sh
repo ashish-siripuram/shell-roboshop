a27b2f0ee1be84b95f7a9a5e801da619b2c7cfab
ashish kumar siripuram <ashishkumarsiripuram1234@gmail.com>
2025-09-25 11:28:01 -0400

    session-06

--- mongodb.sh
+++ mongodb.sh
@@ -30,11 +30,11 @@ VALIDATE(){
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding Mongo repo"
 
-dnf install mongodb-org -y $>>$LOG_FILE
+dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB"
 
-systemctl enable mongod $>>$LOG_FILE
+systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enable MongoDB"
 
-systemctl start mongod $>>$LOG_FILE
+systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Start MongoDB"
