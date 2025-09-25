8692a6cded6491898436294707233178e33d2417
ashish kumar siripuram <ashishkumarsiripuram1234@gmail.com>
2025-09-25 12:36:04 -0400

    session-06

--- mongodb.sh
+++ mongodb.sh
@@ -38,3 +38,9 @@ VALIDATE $? "Enable MongoDB"
 
 systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Start MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongo.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
