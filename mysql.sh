d56949a6581589a850e0239787e44041ee9236fc
ashish kumar siripuram <ashishkumarsiripuram1234@gmail.com>
2025-09-25 15:44:20 -0400

    session-06

---
+++ mysql.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/logs/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+START_TIME=$(date +%s)
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR: Please run this script with root privelege"
+    exit 1 
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install mysql-server -y
+VALIDATE $? "Installing mysql server"
+systemctl enable mysqld
+VALIDATE $? "Enabling mysql server"
+systemctl start mysqld 
+VALIDATE $? "Starting mysql server" 
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up Root password"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME-$START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
